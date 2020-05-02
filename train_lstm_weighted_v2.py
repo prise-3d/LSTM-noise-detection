941485b8af551f6fe95c7cd563c4b7abcd6d9796
Jérôme BUISINE <contact@jeromebuisine.fr>
2020-05-02 18:58:24 +0200

    update model layers

--- train_lstm_weighted_v2.py
+++ train_lstm_weighted_v2.py
@@ -48,11 +48,9 @@ def create_model(input_shape):
     print ('Creating model...')
     model = Sequential()
     #model.add(Embedding(input_dim = 1000, output_dim = 50, input_length=input_length))
-    model.add(GRU(input_shape=input_shape, units=256, activation='sigmoid', recurrent_activation='hard_sigmoid', return_sequences=True))
+    model.add(GRU(input_shape=input_shape, units=512, activation='sigmoid', recurrent_activation='hard_sigmoid', return_sequences=True))
     model.add(Dropout(0.4))
-    model.add(GRU(units=128, activation='sigmoid', recurrent_activation='hard_sigmoid', return_sequences=True))
-    model.add(Dropout(0.4))
-    model.add(GRU(units=64, activation='sigmoid', recurrent_activation='hard_sigmoid'))
+    model.add(GRU(units=256, activation='sigmoid', recurrent_activation='hard_sigmoid', return_sequences=True))
     model.add(Dropout(0.4))
     model.add(Dense(1, activation='sigmoid'))
 
