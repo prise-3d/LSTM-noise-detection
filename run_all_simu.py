b76a974c653fa873f495aebe7ff1f55e701d8fdd
Jérôme BUISINE <contact@jeromebuisine.fr>
2020-11-03 14:23:44 +0100

    update with use of complexity

--- run_all_simu.py
+++ run_all_simu.py
@@ -2,7 +2,7 @@ import os
 import argparse
 
 
-command = "python display/display_thresholds_scene_file.py --params \"{0}\" --method {1} --model {2} --selected_zones {3} --scene {4} --thresholds {5} --seq_norm {6} --sequence {7} --save 1 --save_thresholds {8} --label_thresholds {9} --every {10}"
+command = "python display/display_thresholds_scene_file.py --params \"{0}\" --method {1} --model {2} --selected_zones {3} --scene {4} --thresholds {5} --seq_norm {6} --sequence {7} --save 1 --save_thresholds {8} --label_thresholds {9}"
 
 
 parser = argparse.ArgumentParser(description="Compute simulation for each scenes")
@@ -16,7 +16,6 @@ parser.add_argument('--selected_zones', type=str, help="selected zone file", req
 parser.add_argument('--sequence', type=str, help="sequence size of RNN model", required=True)
 parser.add_argument('--seqnorm', type=str, help="normalization or not of sequence", required=True)
 parser.add_argument('--output', type=str, help="output prediction filename", required=True)
-parser.add_argument('--every', type=int, help='specify if all step images are used or not', default=1)
 
 args = parser.parse_args()
 
@@ -29,12 +28,11 @@ p_selected_zones = args.selected_zones
 p_sequence = args.sequence
 p_seqnorm = args.seqnorm
 p_output = args.output
-p_every  = args.every
 
 
 for scene in sorted(os.listdir(p_folder)):    
     scene_path = os.path.join(p_folder, scene)
-    str_command = command.format(p_params, p_method, p_model, p_selected_zones, scene_path, p_thresholds, p_seqnorm, p_sequence, p_output, scene, p_every)
+    str_command = command.format(p_params, p_method, p_model, p_selected_zones, scene_path, p_thresholds, p_seqnorm, p_sequence, p_output, scene)
 
     print("Run simulation for {0}".format(scene))
     os.system(str_command)
