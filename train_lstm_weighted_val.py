41327b3563ed973cc25fa4a4d3ec793f505bf2f3
Jérôme BUISINE <contact@jeromebuisine.fr>
2020-11-07 13:29:15 +0100

    specific validation subset when training

--- train_lstm_weighted_val.py
+++ train_lstm_weighted_val.py
@@ -83,6 +83,7 @@ def main():
     parser = argparse.ArgumentParser(description="Read and compute training of LSTM model")
 
     parser.add_argument('--train', type=str, help='input train dataset')
+    parser.add_argument('--val', type=str, help='input val dataset')
     parser.add_argument('--test', type=str, help='input test dataset')
     parser.add_argument('--output', type=str, help='output model name')
     parser.add_argument('--epochs', type=int, help='number of expected epochs', default=30)
@@ -93,6 +94,7 @@ def main():
     args = parser.parse_args()
 
     p_train        = args.train
+    p_val          = args.val
     p_test         = args.test
     p_output       = args.output
     p_epochs       = args.epochs
@@ -113,6 +115,7 @@ def main():
     # mkl_set_num_threads(p_cores)
 
     dataset_train = pd.read_csv(p_train, header=None, sep=';')
+    dataset_val = pd.read_csv(p_val, header=None, sep=';')
     dataset_test = pd.read_csv(p_test, header=None, sep=';')
 
     # getting weighted class over the whole dataset
@@ -121,13 +124,18 @@ def main():
     nb_noisy_train = len(noisy_df_train.index)
     nb_not_noisy_train = len(not_noisy_df_train.index)
 
+    noisy_df_val = dataset_val[dataset_val.iloc[:, 0] == 1]
+    not_noisy_df_val = dataset_val[dataset_val.iloc[:, 0] == 0]
+    nb_noisy_val = len(noisy_df_val.index)
+    nb_not_noisy_val = len(not_noisy_df_val.index)
+
     noisy_df_test = dataset_test[dataset_test.iloc[:, 0] == 1]
     not_noisy_df_test = dataset_test[dataset_test.iloc[:, 0] == 0]
     nb_noisy_test = len(noisy_df_test.index)
     nb_not_noisy_test = len(not_noisy_df_test.index)
 
-    noisy_samples = nb_noisy_test + nb_noisy_train
-    not_noisy_samples = nb_not_noisy_test + nb_not_noisy_train
+    noisy_samples = nb_noisy_test + nb_noisy_val + nb_noisy_train
+    not_noisy_samples = nb_not_noisy_test + nb_not_noisy_val + nb_not_noisy_train
 
     total_samples = noisy_samples + not_noisy_samples
 
@@ -142,12 +150,17 @@ def main():
 
     # shuffle data
     final_df_train = sklearn.utils.shuffle(dataset_train)
+    final_df_val = sklearn.utils.shuffle(dataset_val)
     final_df_test = sklearn.utils.shuffle(dataset_test)
 
     # split dataset into X_train, y_train, X_test, y_test
     X_train_all = final_df_train.loc[:, 1:].apply(lambda x: x.astype(str).str.split(' '))
     X_train_all = build_input(X_train_all, p_seq_norm)
     y_train_all = final_df_train.loc[:, 0].astype('int')
+
+    X_val_all = final_df_val.loc[:, 1:].apply(lambda x: x.astype(str).str.split(' '))
+    X_val_all = build_input(X_val_all, p_seq_norm)
+    y_val_all = final_df_val.loc[:, 0].astype('int')
 
     X_test = final_df_test.loc[:, 1:].apply(lambda x: x.astype(str).str.split(' '))
     X_test = build_input(X_test, p_seq_norm)
@@ -159,10 +172,10 @@ def main():
     model.summary()
 
     # prepare train and validation dataset
-    X_train, X_val, y_train, y_val = train_test_split(X_train_all, y_train_all, test_size=0.3, shuffle=False)
+    #X_train, X_val, y_train, y_val = train_test_split(X_train_all, y_train_all, test_size=0.3, shuffle=False)
 
     print("Fitting model with custom class_weight", class_weight)
-    history = model.fit(X_train, y_train, batch_size=p_batch_size, epochs=p_epochs, validation_data=(X_val, y_val), verbose=1, shuffle=True, class_weight=class_weight)
+    history = model.fit(X_train_all, y_train_all, batch_size=p_batch_size, epochs=p_epochs, validation_data=(X_val_all, y_val_all), verbose=1, shuffle=True, class_weight=class_weight)
 
     # list all data in history
     print(history.history.keys())
@@ -186,19 +199,19 @@ def main():
     # train_score, train_acc = model.evaluate(X_train, y_train, batch_size=1)
 
     # print(train_acc)
-    y_train_predict = [ 1 if x > 0.5 else 0 for x in model.predict(X_train) ]
-    y_val_predict = [ 1 if x > 0.5 else 0 for x in model.predict(X_val) ]
+    y_train_predict = [ 1 if x > 0.5 else 0 for x in model.predict(X_train_all) ]
+    y_val_predict = [ 1 if x > 0.5 else 0 for x in model.predict(X_val_all) ]
     y_test_predict = [ 1 if x > 0.5 else 0 for x in model.predict(X_test) ]
 
     # print(y_train_predict)
     # print(y_test_predict)
 
-    auc_train = roc_auc_score(y_train, y_train_predict)
-    auc_val = roc_auc_score(y_val, y_val_predict)
+    auc_train = roc_auc_score(y_train_all, y_train_predict)
+    auc_val = roc_auc_score(y_val_all, y_val_predict)
     auc_test = roc_auc_score(y_test, y_test_predict)
 
-    acc_train = accuracy_score(y_train, y_train_predict)
-    acc_val = accuracy_score(y_val, y_val_predict)
+    acc_train = accuracy_score(y_train_all, y_train_predict)
+    acc_val = accuracy_score(y_val_all, y_val_predict)
     acc_test = accuracy_score(y_test, y_test_predict)
     
     print('Train ACC:', acc_train)
