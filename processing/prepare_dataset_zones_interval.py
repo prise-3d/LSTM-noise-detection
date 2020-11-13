ead4756f5eee85728468183e433627577b275645
Jérôme BUISINE <contact@jeromebuisine.fr>
2020-11-13 17:32:38 +0100

    prepare for categorical training process

---
+++ processing/prepare_dataset_zones_interval.py
@@ -0,0 +1,256 @@
+# main imports
+import numpy as np
+import pandas as pd
+import sys, os, argparse
+import random
+
+# image processing
+from PIL import Image
+from ipfml import utils
+from ipfml.processing import transform, segmentation
+
+# modules and config imports
+sys.path.insert(0, '') # trick to enable import of main folder module
+
+import custom_config as cfg
+
+dataset_folder = cfg.dataset_path
+scenes_list    = cfg.scenes_names
+zones_indices  = cfg.zones_indices
+
+'''
+Display progress information as progress bar
+'''
+def write_progress(progress):
+    barWidth = 180
+
+    output_str = "["
+    pos = barWidth * progress
+    for i in range(barWidth):
+        if i < pos:
+           output_str = output_str + "="
+        elif i == pos:
+           output_str = output_str + ">"
+        else:
+            output_str = output_str + " "
+
+    output_str = output_str + "] " + str(int(progress * 100.0)) + " %\r"
+    print(output_str)
+    sys.stdout.write("\033[F")
+
+def save_learned_zones(output_name, scene, zones):
+
+    if not os.path.exists(cfg.output_zones_learned):
+        os.makedirs(cfg.output_zones_learned)
+
+    with open(os.path.join(cfg.output_zones_learned, output_name), 'a') as f:
+        f.write(scene + ';')
+
+        for zone in zones:
+            f.write(str(zone) + ';')
+
+        f.write('\n')
+
+
+def get_random_zones(scene, zones, n_zones):
+
+    random.shuffle(zones)
+
+    # specific case for 'Cuisine01' (zone 12 is also noisy even in reference image)
+    if scene == 'Cuisine01':
+        while 12 in zones[0:n_zones]:
+            random.shuffle(zones)
+    
+    return zones[0:n_zones]
+
+def main():
+
+    parser = argparse.ArgumentParser(description="Read and compute entropy data file (using diff)")
+
+    parser.add_argument('--data', type=str, help='file data to read and compute')
+    parser.add_argument('--output', type=str, help='output dataset prefix file used (saved into .train and .test extension)')
+    parser.add_argument('--thresholds', type=str, help='file which contains all thresholds with min max values', required=True)
+    parser.add_argument('--sequence', type=int, help='sequence length expected')
+    parser.add_argument('--n_zones', type=int, help='number of zones used in train', default=0)
+    parser.add_argument('--selected_zones', type=str, help='file with specific training zone')
+    parser.add_argument('--seq_norm', type=int, help='normalization sequence by features', choices=[0, 1])
+
+    args = parser.parse_args()
+
+    p_data         = args.data
+    p_output       = args.output
+    p_thresholds   = args.thresholds
+    p_sequence     = args.sequence
+    p_n_zones      = args.n_zones
+    p_selected_zones = args.selected_zones
+    p_seq_norm     = bool(args.seq_norm)
+
+    learned_zones = None
+
+    if p_selected_zones is not None:
+        print('Use of specific learned zones')
+
+        with open(p_selected_zones, 'r') as f:
+            lines = f.readlines()
+
+            learned_zones = {}
+
+            for line in lines:
+                data = line.split(';')
+                del data[-1]
+                print(data)
+                learned_zones[data[0]] = [ int(d) for d in data[1:] ]
+    else:
+        print("Number of zones used in train:", p_n_zones)
+
+    thresholds = {}
+
+    with open(p_thresholds) as f:
+        thresholds_line = f.readlines()
+
+        for line in thresholds_line:
+            data = line.split(';')
+            del data[-1] # remove unused last element `\n`
+            scene = data[0]
+            thresholds_scene = [ list(map(int, l.split(' '))) for l in data[1:] ]
+
+            thresholds[scene] = thresholds_scene
+
+    print(thresholds)
+
+    # create output path if not exists
+    p_output_path = os.path.join(cfg.output_datasets, p_output)
+    p_output_path_train = os.path.join(p_output_path, p_output + '.train')
+    p_output_path_test = os.path.join(p_output_path, p_output + '.test')
+
+    if not os.path.exists(p_output_path):
+        os.makedirs(p_output_path)
+
+    # read line by line file to estimate threshold entropy stopping criteria
+    f_train = open(p_output_path_train, 'w')
+    f_test = open(p_output_path_test, 'w')
+
+    zones = np.arange(16)
+
+    with open(p_data, 'r') as f:
+        lines = f.readlines()
+
+        nlines = len(lines)
+        ncounter = 0
+
+        new_scene = False
+        current_scene = None
+        selected_zones = None
+
+        for line in lines:
+
+            data = line.split(';')
+
+            # only if scene is used for training part
+            scene_name = data[0]
+            zones_index = int(data[1])
+            #threshold = int(data[3])
+            image_indices = data[4].split(',')
+            values_list = data[5].split(',')
+
+            sequence_data = []
+            # one element is removed using this function (first element of list for computing first difference)
+            # TODO : remove previous and add new
+
+            if current_scene == None:
+                new_scene == True
+                current_scene = scene_name
+
+                # check if use of selected zones
+                if learned_zones:
+                    selected_zones = learned_zones[scene_name]
+                else:
+                    selected_zones = get_random_zones(scene_name, zones, p_n_zones)
+
+                save_learned_zones(p_output, scene_name, selected_zones)
+
+            if scene_name != current_scene:
+                new_scene = True
+                random.shuffle(zones)
+                
+                # check if use of selected zones
+                if learned_zones:
+                    selected_zones = learned_zones[scene_name]
+                else:
+                    selected_zones = get_random_zones(scene_name, zones, p_n_zones)
+
+                save_learned_zones(p_output, scene_name, selected_zones)
+            else:
+                new_scene = False
+
+            current_scene = scene_name
+
+            for i, index in enumerate(image_indices):
+                
+                values = values_list[i].split(' ')
+
+                # append new sequence
+                sequence_data.append(values)
+
+                # save additionnals information
+                # scene_name; zone_id; image_index_end; label; data
+                if i + 1 >= p_sequence:
+                    
+                    # check before min
+                    if int(index) <= thresholds[scene_name][zones_index][0]:
+                        label = 1
+                    # check after max
+                    elif int(index) > thresholds[scene_name][zones_index][-1]:
+                        label = 0
+                    else:
+                        label = 2 # transition label between noisy and not noisy (min and max) (incertitude part)
+
+                    line = scene_name + ';'
+                    line += str(zones_index) + ';'
+                    line += str(index) + ';'
+                    line += str(label) + ';'
+
+                    current_sequence_data = sequence_data.copy()
+
+                    # do seq normalisation here if necessary
+                    if p_seq_norm:
+                        
+                        data = np.array(current_sequence_data, 'float32')
+                        _, f = data.shape
+                        for i in range(f):
+                            #final_arr[index][]
+                            data[:, i] = utils.normalize_arr_with_range(data[:, i])
+
+                        current_sequence_data = data.tolist()
+                    
+                    for index_v, values in enumerate(current_sequence_data):
+
+                        for index_x, x in enumerate(values):
+                            line += str(x)
+
+                            if index_x + 1 < len(values):
+                                line += ' '
+
+                        if index_v + 1 < len(current_sequence_data):
+                            line += ';'
+
+                    line += '\n'
+
+                    if zones_index in selected_zones:
+                        f_train.write(line)
+                    else:
+                        f_test.write(line)
+
+                    # del previous element
+                    del sequence_data[0]
+
+            ncounter += 1
+            write_progress(float(ncounter / nlines))
+
+    f_test.close()
+    f_train.close()    
+    
+
+
+if __name__== "__main__":
+    main()
