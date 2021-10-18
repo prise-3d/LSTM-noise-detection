90fca1d6c4e088417cd4b29124c1e0c8c8b64ed5
Jérôme BUISINE <contact@jeromebuisine.fr>
2021-10-18 16:45:13 +0200

    Enable predictions over EXR

---
+++ predictions/display_prediction_zone_threshold.py
@@ -0,0 +1,250 @@
+# main imports
+import numpy as np
+import pandas as pd
+import sys, os, argparse
+
+# image processing
+from PIL import Image
+from ipfml import utils
+from ipfml.processing import transform, segmentation
+
+import matplotlib.pyplot as plt
+
+# model imports
+import joblib
+
+# modules and config imports
+sys.path.insert(0, '') # trick to enable import of main folder module
+
+import custom_config as cfg
+from modules.utils import data as dt
+
+def write_progress(progress):
+    barWidth = 180
+
+    output_str = "["
+    pos = barWidth * progress
+    for i in range(barWidth):
+        if i < pos:
+           output_str = output_str + "="
+        elif i == pos:
+           output_str = output_str + ">"
+        else:
+            output_str = output_str + " "
+
+    output_str = output_str + "] " + str(int(progress * 100.0)) + " %\r"
+    print(output_str)
+    sys.stdout.write("\033[F")
+
+
+def specific_display_label(label, chunk_size=3):
+    label = label[::-1] # reverse label
+    labels = [ label[i:i+chunk_size] for i in range(0, len(label), chunk_size) ]
+    return ' '.join(labels)[::-1]
+
+def display_simulation_thresholds(scene, zones_predictions, humans, image_indices, output, n_predictions, nsamples, p_zone, p_threshold, every=None, zones_learned=None):
+    
+    # get reference image
+    fig=plt.figure(figsize=(35, 22))
+    # fig.suptitle("Detection simulation for " + scene + " scene", fontsize=20)
+
+    # dataset information
+    start_index = int(image_indices[1]) - int(image_indices[0])
+    step_value = int(image_indices[1]) - int(image_indices[0])
+
+    label_freq = (nsamples / step_value / 100 * 3) - 1
+
+    if every is not None:
+        step_value = every * step_value
+    
+    if every == 1:
+        label_freq = 2 * label_freq
+
+    y_min_lim, y_max_lim = (-0.2, 1.2)
+
+    prediction_found = []
+
+    predictions_data = zones_predictions[p_zone]
+
+    predictions = []
+    predictions_label = []
+
+    threshold_model = None
+    predictions_counter = 0
+
+    for index_v, v in enumerate(predictions_data):
+        v = float(v)
+        predictions.append(v)
+        predictions_label.append([0 if v < p_threshold else 1])
+
+        if threshold_model is None:
+            if v < p_threshold:
+                predictions_counter += 1
+
+                if predictions_counter >= n_predictions:
+                    threshold_model = index_v
+                    prediction_found.append(index_v * step_value + start_index)
+            else:
+                predictions_counter = 0
+
+    
+    # get index of current value
+    counter_index = 0
+    current_value = start_index
+
+    while(current_value < humans[p_zone]):
+        counter_index += 1
+        current_value += step_value
+
+    # fig.add_subplot(4, 4, (index + 1))
+    plt.plot(predictions, lw=6)
+    plt.plot(predictions_label, linestyle='--', color='slategray', lw=4)
+    #plt.imshow(blocks[index], extent=[0, len(predictions), y_min_lim, y_max_lim])
+
+    # if zones_learned is not None:
+    #     if p_zone in zones_learned:
+    #         ax = plt.gca()
+            # ax.set_facecolor((0.9, 0.95, 0.95))
+
+    if threshold_model is None:
+        threshold_model = len(predictions_data) - 1
+        prediction_found.append(len(predictions_data) * step_value)
+
+    # draw vertical line from (70,100) to (70, 250)
+    plt.plot([counter_index, counter_index], [-2, 2], 'k-', lw=10, color='red')
+    plt.plot([threshold_model, threshold_model], [-2, 2], 'k-', lw=8, color='blue')
+    plt.plot([ pthreshold for i in range(len(predictions))], lw=2, color='orange', linestyle='--')
+
+#        if index % 4 == 0:
+    plt.ylabel('Bruité / Non bruité', fontsize=70)
+
+#        if index >= 12:
+    plt.xlabel('Échantillons par pixel', fontsize=70)
+
+    x_labels = [id * step_value + start_index for id, val in enumerate(predictions) if id % label_freq == 0]  + [nsamples]
+    #x_labels = [id * step_value + start_index for id, val in enumerate(predictions) if id % label_freq == 0]
+
+    x = [v for v in np.arange(0, len(predictions)) if v % label_freq == 0] + [int(nsamples / (20 * every))]
+    y = np.arange(-1, 2, 10)
+
+    plt.xticks(x, [ specific_display_label(str(x)) for x in x_labels ], rotation=45, fontsize=56)
+    
+    ax = plt.gca()
+    for label in (ax.get_xticklabels() + ax.get_yticklabels()):
+        label.set_fontsize(56)
+
+    ax.tick_params(width=5, length=15, size=15)
+    # ax[1].tick_params(width=3, length=6)
+
+    plt.ylim(y_min_lim, y_max_lim)
+
+    fig.tight_layout()
+    plt.savefig(output + '.pdf', dpi=100)
+    #plt.show()()
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description="Read and process prediction file")
+
+    parser.add_argument('--predictions', type=str, help='prediction file of scene')
+    parser.add_argument('--npredictions', type=int, help='expected number of predictions before gettint threshold')
+    parser.add_argument('--sequence', type=int, help='sequence length expected')
+    parser.add_argument('--pthreshold', type=float, help='sequence length expected')
+    parser.add_argument('--thresholds', type=str, help='file which cantains all thresholds')
+    parser.add_argument('--learned_zones', type=str, help="Filename which specifies if zones are learned or not and which zones", default="")
+    parser.add_argument('--every', type=int, help="every images only", default=1)
+    parser.add_argument('--scene', type=str, help='Scene folder to use')
+    parser.add_argument('--nsamples', type=int, help='expected max number of samples')
+    parser.add_argument('--zone', type=int, help='expected zone number')
+    parser.add_argument('--output', type=str, help='output image file name')
+
+    args = parser.parse_args()
+
+    p_prediction = args.predictions
+    p_n_prediction = args.npredictions
+    p_sequence = args.sequence
+    p_pthreshold = args.pthreshold
+    p_thresholds = args.thresholds
+    p_zones    = args.learned_zones
+    p_every    = args.every
+    p_scene    = args.scene
+    p_output   = args.output
+    p_zone     = args.zone
+    p_nsamples = args.nsamples
+
+    # 1. get scene name
+    scene_path = p_scene
+
+    if scene_path[-1] == '/':
+        scene_path = scene_path[:-1]
+
+    _, scene_name = os.path.split(scene_path)
+
+
+    human_thresholds = []
+
+    # 3. retrieve human_thresholds
+    # construct zones folder
+    with open(p_thresholds) as f:
+        thresholds_line = f.readlines()
+
+        for line in thresholds_line:
+            data = line.split(';')
+            del data[-1] # remove unused last element `\n`
+            current_scene = data[0]
+            thresholds_scene = data[1:]
+
+            if scene_name == current_scene:
+                human_thresholds = [ int(threshold) for threshold in  thresholds_scene ]
+
+    if len(human_thresholds) == 0:
+        print('Cannot manage this scene, no thresholds available')
+        return
+    
+    print(human_thresholds)
+    
+    images_path = sorted([os.path.join(scene_path, img) for img in os.listdir(scene_path) if cfg.scene_image_extension in img])
+    number_of_images = len(images_path)
+    image_indices = [ dt.get_scene_image_quality(img_path) for img_path in images_path ]
+
+    zones_predictions = []
+    # Get predictions from model (let by default to 1 the first sequence values)
+    with open(p_prediction, 'r') as f:
+        lines = f.readlines()
+
+        for line in lines:
+            data = line.split(';')
+
+            if data[0] == scene_name:
+
+                predictions = []
+                
+                for _ in range(p_sequence - 1):
+                    predictions.append(1)
+
+                for v in data[2:-1]:
+                    predictions.append(v)
+
+                zones_predictions.append(predictions)
+
+    # 5. check if learned zones
+    zones_learned = None
+
+    if len(p_zones) > 0:
+        with open(p_zones, 'r') as f:
+            lines = f.readlines()
+
+            for line in lines:
+                data = line.split(';')
+
+                if data[0] == scene_name:
+                    zones_selected = data[1:]
+                    del zones_selected[-1]
+                    zones_learned = [ int(zone) for zone in zones_selected ]
+
+    # 6. display results
+    display_simulation_thresholds(scene_name, zones_predictions, human_thresholds, image_indices, p_output, p_n_prediction, p_nsamples, p_zone, p_pthreshold, p_every, zones_learned)
+
+if __name__== "__main__":
+    main()
---
+++ predictions/estimate_predictions_exr.py
@@ -0,0 +1,221 @@
+# main imports
+import numpy as np
+import pandas as pd
+import sys, os, argparse
+
+# image processing
+from PIL import Image
+from ipfml import utils
+import Imath
+import OpenEXR
+from ipfml.processing import transform, segmentation
+
+import matplotlib.pyplot as plt
+
+# model imports
+import joblib
+from keras.models import load_model
+
+# modules and config imports
+sys.path.insert(0, '') # trick to enable import of main folder module
+
+import custom_config as cfg
+from modules.utils import data as dt
+
+from processing.features_extractions import extract_data
+
+dataset_folder = cfg.dataset_path
+scenes_list    = cfg.scenes_names
+zones_indices  = cfg.zones_indices
+
+output_figures = cfg.output_figures
+
+def write_progress(progress):
+    barWidth = 180
+
+    output_str = "["
+    pos = barWidth * progress
+    for i in range(barWidth):
+        if i < pos:
+           output_str = output_str + "="
+        elif i == pos:
+           output_str = output_str + ">"
+        else:
+            output_str = output_str + " "
+
+    output_str = output_str + "] " + str(int(progress * 100.0)) + " %\r"
+    print(output_str)
+    sys.stdout.write("\033[F")
+
+def get_center_image(img, width=200, height=200):
+
+    _, w, h = img.shape
+    return img[:, int(w/2) - int(width/2):int(w/2) + int(width/2), int(h/2) - int(height/2):int(h/2) + int(height/2)]
+
+def read_image(img_path):
+
+    extension = img_path.split('.')[-1]
+
+    if extension == 'exr':
+        
+        if not OpenEXR.isOpenExrFile(img_path):
+            raise ValueError(f'Image {img_path} is not a valid OpenEXR file')
+
+        src = OpenEXR.InputFile(img_path)
+        pixel_type = Imath.PixelType(Imath.PixelType.FLOAT)
+        dw = src.header()['dataWindow']
+        size = (dw.max.x - dw.min.x + 1, dw.max.y - dw.min.y + 1)
+        
+        # Read into numpy array
+        #img = np.zeros((3, size[1], size[0]))
+        img_channels = []
+        for i, c in enumerate('RGB'):
+            rgb32f = np.fromstring(src.channel(c, pixel_type), dtype=np.float32)
+            #img[i, :, :] = rgb32f.reshape(size[1], size[0])
+            img_channels.append(rgb32f.reshape(size[1], size[0]))
+
+        img_data = np.array(img_channels)
+        img_data = np.array(img_data / np.max(img_data), 'float32')
+
+        img_data = get_center_image(img_data)
+
+        return img_data
+            
+    if extension == 'png':
+        img_data = Image.open(img_path)
+        img_data = np.moveaxis(img_data, -1, 0)
+        return np.array(img_data / 255., 'float32')
+
+    return None
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description="Read and compute entropy data file")
+
+    parser.add_argument('--model', type=str, help='entropy file data with estimated threshold to read and compute')
+    parser.add_argument('--method', type=str, help='method name to used', choices=cfg.features_choices_labels, default=cfg.features_choices_labels[0])
+    parser.add_argument('--params', type=str, help='param of the method used', default="")
+    parser.add_argument('--sequence', type=int, help='sequence length expected')
+    parser.add_argument('--imnorm', type=int, help="specify if image is normalized before computing something", default=0, choices=[0, 1])
+    parser.add_argument('--scenes', type=str, help='Scenes names to use')
+    parser.add_argument('--dataset', type=str, help='dataset folder to use')
+    parser.add_argument('--seq_norm', type=int, help='normalization sequence by features', choices=[0, 1])
+    parser.add_argument('--every', type=int, help="every images only", default=1)
+    parser.add_argument('--output', type=str, help="output prediction file")
+
+    args = parser.parse_args()
+
+    p_model      = args.model
+    p_method     = args.method
+    p_params     = args.params
+    p_sequence   = args.sequence
+    p_imnorm     = args.imnorm
+    p_dataset    = args.dataset
+    p_scenes      = args.scenes
+    p_seq_norm   = bool(args.seq_norm)
+    p_every  = args.every
+    p_output     = args.output
+
+
+    # scene path by default
+    scenes_list = []
+
+    with open(p_scenes, 'r') as f:
+        for l in f.readlines():
+            scenes_list.append(l.split(';')[0])
+
+    # 2. load model and compile it
+    model = load_model(p_model)
+    model.compile(loss='binary_crossentropy',
+                  optimizer='rmsprop',
+                  metrics=['accuracy'])
+
+    # 4. get estimated thresholds using model and specific method
+    for s_i, scene in enumerate(sorted(scenes_list)):
+        
+        scenes_images = []
+
+        # store all expected prediction for all zones of the image
+        model_predictions = []
+
+        print('----')
+        print(f'    -- Load data for scene {s_i + 1} of {len(scenes_list)}...')
+
+        scene_path = os.path.join(p_dataset, scene)
+
+        images_names = sorted(os.listdir(scene_path))
+
+        # read all images of scene
+        for img in images_names:
+            img_path = os.path.join(scene_path, img)
+            scenes_images.append(img_path)
+   
+        blocks_sequence = []
+        blocks_predictions = []
+        image_counter = 0
+
+        # append empty list
+
+        for img_i, img_path in enumerate(images_names):
+
+            block = read_image(img_path)
+   
+            # normalize if necessary
+            if p_imnorm:
+                block = np.array(block) / 255.
+
+            blocks_sequence.append(np.array(extract_data(block, p_method, p_params)))
+
+            # check if prediction is possible
+            if len(blocks_sequence) >= p_sequence:
+                data = np.array(blocks_sequence)
+                
+                if data.ndim == 1:
+                    data = data.reshape(len(blocks_sequence), 1)
+                else:
+                    # check if sequence normalization is used
+                    if p_seq_norm:
+
+                        # check snorm process
+                        #for _, seq in enumerate(data):
+                            
+                        s, f = data.shape
+                        for i in range(f):
+                            #final_arr[index][]
+                            data[:, i] = utils.normalize_arr_with_range(data[:, i])
+                                
+                data = np.expand_dims(data, axis=0)
+                
+                prob = model.predict(data, batch_size=1)[0][0]
+                #print(index, ':', image_indices[img_i], '=>', prob)
+
+                blocks_predictions.append(prob)
+
+                #print('Block @', index, ':', len(blocks_sequence[index]))
+                # delete first element (just like sliding window)
+                del blocks_sequence[0]
+
+            # write progress bar
+            write_progress((image_counter + 1) / len(images_names))
+            
+            image_counter = image_counter + 1
+        
+
+        # 6. display results
+        if not os.path.exists(p_output):
+            os.makedirs(p_output)
+
+        # be default only one zone
+        with open(os.path.join(p_output, 'predictions.csv'), 'a') as f:
+            
+            f.write(f'{scene};0')
+
+            for v in model_predictions:
+                f.write(f';{v}')
+                    
+            f.write('\n')
+
+
+if __name__== "__main__":
+    main()
